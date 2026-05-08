db12344a7a3a72b209146848fee168aa6ae790e7
CentralBureauFundCBFNigeriaGrant <hon.olanrewajurichard@gmail.com>
2026-05-08 15:45:07 +0100

    Update video list in stream.py
    Removed old video URLs and added new ones for streaming.

--- stream.py
+++ stream.py
@@ -1,30 +1,27 @@
 import subprocess, time
 
-STREAM_KEY = "1dr5-f1jq-bspa-qbee-2yda"
-
+STREAM_KEY = "YOUR_STREAM_KEY"
 VIDEO_LIST = [
     "https://github.com/CentralBureauFundCBFNigeriaGrant/marveltube-stream/releases/download/V2.0/I.Tried.Cooking.For.My.Wife.in.10.Minutes.I.FAILED.720p._1778229740240.mp4",
-    "https://github.com/CentralBureauFundCBFNigeriaGrant/marveltube-stream/releases/download/V2.0/Does.Your.Background.Really.Matter.To.Go.Viral.on.YouTube._.A.MUST.WATCH.FOR.NEW.CREATORS.1080p._1778228673635.mp4"
+    "https://github.com/CentralBureauFundCBFNigeriaGrant/marveltube-stream/releases/download/V2.0/Does.Your.Background.Really.Matter.To.Go.Viral.on.YouTube._.A.MUST.WATCH.FOR.NEW.CREATORS.1080p._1778228673635.mp4",
 ]
 
-# Write playlist
 with open("playlist.txt", "w") as f:
     for url in VIDEO_LIST:
         f.write(f"file '{url}'\n")
 
-# Simple infinite loop – restart FFmpeg after each complete run
 while True:
     cmd = [
-        "ffmpeg", "-re",
-        "-f", "concat",
-        "-safe", "0",
-        "-protocol_whitelist", "file,https,tcp,tls",
-        "-stream_loop", "-1",
+        "ffmpeg", "-re", "-stream_loop", "-1", "-f", "concat",
         "-i", "playlist.txt",
-        "-c", "copy",
-        "-f", "flv",
-        f"rtmp://a.rtmp.youtube.com/live2/{STREAM_KEY}"
+        "-vf", "scale=1280:-2",
+        "-c:v", "libx264", "-preset", "superfast",
+        "-b:v", "2000k", "-maxrate", "2000k", "-bufsize", "4000k",
+        "-g", "60", "-keyint_min", "60",
+        "-r", "30", "-pix_fmt", "yuv420p",
+        "-c:a", "aac", "-b:a", "128k", "-ar", "44100",
+        "-f", "flv", f"rtmp://a.rtmp.youtube.com/live2/{STREAM_KEY}"
     ]
     process = subprocess.run(cmd)
-    print(f"Stream ended (exit code {process.returncode}). Restarting in 5 seconds...")
+    print("Stream crashed. Restarting in 5s...")
     time.sleep(5)
